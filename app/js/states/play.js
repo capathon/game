5a7f1cf3fd1e32a4420c2bd7e3ed40b0c572e770
Arjen <arjen.brandenburgh@gmail.com>
2015-02-20 22:06:41 +0100

    Re-fixed ledges and first level implementation

--- app/js/states/play.js
+++ app/js/states/play.js
@@ -19,6 +19,38 @@ BasicGame.Game = function (game) {
     this.particles; //  the particle manager (Phaser.Particles)
     this.physics;   //  the physics manager (Phaser.Physics)
     this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+    this.levels = [{},
+        {
+            level: 1,
+            backgroundAutoScroll: -20,
+            condomTimer: 1.25,
+            virusTimer: 1.50
+        }, {
+            level: 2,
+            pointsToReach: 45,
+            backgroundAutoScroll: -30,
+            condomTimer: 1.25,
+            virusTimer: 1.50
+        }, {
+            level: 3,
+            pointsToReach: 90,
+            backgroundAutoScroll: -40,
+            condomTimer: 1.25,
+            virusTimer: 1.50
+        }, {
+            level: 4,
+            pointsToReach: 30,
+            backgroundAutoScroll: -50,
+            condomTimer: 1.25,
+            virusTimer: 1.50
+        }, {
+            level: 5,
+            pointsToReach: 30,
+            backgroundAutoScroll: -60,
+            condomTimer: 1.25,
+            virusTimer: 1.50
+        }
+    ];
 
     //  You can use any of these from any function within this State.
     //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
@@ -34,11 +66,12 @@ BasicGame.Game.prototype = {
         // give our world an initial gravity of 1200
         this.game.physics.arcade.gravity.y = 1200;
 
-
+        // we start at level 1
+        this.level = 1;
 
         // add the background sprite, also as a tile
         this.background = this.game.add.tileSprite(0,0,this.game.width,this.game.height,'background');
-        this.background.autoScroll(-20,0);
+        this.background.autoScroll(this.levels[this.level].backgroundAutoScroll, 0);
 
         // add the ground sprite as a tile
         // and start scrolling in the negative x direction
@@ -54,11 +87,12 @@ BasicGame.Game.prototype = {
         this.virusses = this.game.add.group();
 
 
+        // create and add a group to hold our virusGroup prefabs
+        this.ledges = this.game.add.group();
+
         // create and add a new Player object
         this.player = new Player(this.game, this.game.width/2, this.game.height/2);
         this.game.add.existing(this.player);
-
-
 
 
 
@@ -121,6 +155,12 @@ BasicGame.Game.prototype = {
             this.virusses.forEach(function(virusGroup) {
                 this.game.physics.arcade.collide(this.player, virusGroup, this.pickUpVirus, null, this);
             }, this);
+
+
+            // enable collisions between the player and each group in the ledges group
+            this.ledges.forEach(function(ledgeGroup) {
+                this.game.physics.arcade.collide(this.player, ledgeGroup, this.touchedGround, null, this);
+            }, this);
         }
     },
     shutdown: function() {
@@ -128,6 +168,7 @@ BasicGame.Game.prototype = {
         this.player.destroy();
         this.condoms.destroy();
         this.virusses.destroy();
+        this.ledges.destroy();
         // this.questionmodal.destroy();
     },
     startGame: function() {
@@ -135,10 +176,12 @@ BasicGame.Game.prototype = {
             this.player.body.allowGravity = true;
             this.player.alive = true;
             // add a timer
-            this.condomGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 1.25, this.generateCondoms, this);
+            this.condomGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * this.levels[this.level].condomTimer, this.generateCondoms, this);
             this.condomGenerator.timer.start();
-            this.virusGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 1.50, this.generateVirusses, this);
+            this.virusGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * this.levels[this.level].virusTimer, this.generateVirusses, this);
             this.virusGenerator.timer.start();
+            this.ledgeGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 5.0, this.generateLedges, this);
+            this.ledgeGenerator.timer.start();
 
             this.instructionGroup.destroy();
         }
@@ -181,6 +224,8 @@ BasicGame.Game.prototype = {
             this.condomGenerator.timer.stop();           
             this.virusses.callAll('stop');
             this.virusGenerator.timer.stop();
+            this.ledges.callAll('stop');
+            this.ledgeGenerator.timer.stop();
             this.ground.stopScroll();
         }
 
@@ -202,9 +247,8 @@ BasicGame.Game.prototype = {
         this.player.numberOfJumps = 0;
     },
     generateCondoms: function() {
-        var quarterScreen = this.game.height/4;
+        var quarterScreen = this.game.height / 4;
         var condomY = this.game.rnd.integerInRange(-quarterScreen, quarterScreen*1.5);
-        console.log(condomY);
         var condomGroup = this.condoms.getFirstExists(false);
         if(!condomGroup) {
             condomGroup = new CondomGroup(this.game, this.condoms);
@@ -212,7 +256,7 @@ BasicGame.Game.prototype = {
         condomGroup.reset(this.game.width -10, condomY);
     },
     generateVirusses: function() {
-        var quarterScreen = this.game.height/4;
+        var quarterScreen = this.game.height / 4;
         var virusY = this.game.rnd.integerInRange(-quarterScreen, (2*quarterScreen)-63);
         var virusGroup = this.virusses.getFirstExists(false);
         if(!virusGroup) {
@@ -220,9 +264,19 @@ BasicGame.Game.prototype = {
         }
         virusGroup.reset(this.game.width, virusY);
     },
+    generateLedges: function() {
+        var quarterScreen = this.game.height / 4;
+        var ledgeY = this.game.rnd.integerInRange(-quarterScreen, (2*quarterScreen)-63);
+        var ledgeGroup = this.ledges.getFirstExists(false);
+        if(!ledgeGroup) {
+            ledgeGroup = new LedgeGroup(this.game, this.ledges);
+        }
+        ledgeGroup.reset(this.game.width, ledgeY);
+    },
     pauseGame: function () {
         this.condoms.destroy();
         this.virusses.destroy();
+        this.ledges.destroy();
 
         this.condoms.callAll('stop');
         this.condomGenerator.timer.stop();  
@@ -230,6 +284,9 @@ BasicGame.Game.prototype = {
         this.virusses.callAll('stop');
         this.virusGenerator.timer.stop();
         
+        this.ledges.callAll('stop');
+        this.ledgeGenerator.timer.stop();
+
         this.ground.stopScroll();
 
         this.player.animations.stop();
@@ -237,6 +294,7 @@ BasicGame.Game.prototype = {
     resumeGame: function () {
         this.condoms = this.game.add.group();
         this.virusses = this.game.add.group();
+        this.ledges = this.game.add.group();
 
         this.ground.autoScroll(-200,0);
 
@@ -244,6 +302,10 @@ BasicGame.Game.prototype = {
         this.condomGenerator.timer.start();
         this.virusGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 1.50, this.generateVirusses, this);
         this.virusGenerator.timer.start();
+        this.ledgeGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 1.50, this.generateLedges, this);
+        this.ledgeGenerator.timer.start();
+        this.ledgesGenerator = this.game.time.events.loop(Phaser.Timer.SECOND * 5.00, this.generateLedges, this);
+        this.ledgesGenerator.timer.start();
 
         this.player.animations.play('jump', 12, true);
     }
