7fc5847350bcf3763f583a344c187953a9ff48c4
thomasdelissen <thomasdelissen1@gmail.com>
2015-02-21 14:42:43 +0100

    Added CUP to final screen

--- app/js/prefabs/endModal.js
+++ app/js/prefabs/endModal.js
@@ -2,9 +2,10 @@ var EndModal = function(game, parent) {
   Phaser.Group.call(this, game, parent);
 // Add the modal
 //  this.endmodal = this.create(this.game.width / 2, -400, 'endmodal');
-  this.endmodal = this.create(0, 0, 'endmodal');
-  this.endmodal.anchor.setTo(0.5, 0.5);
-
+  this.endmodal = this.create(-70, -50, 'cup');
+  this.endmodal.anchor.setTo(0, 0);
+  this.endmodal.height = 140;
+  this.endmodal.width = 140;
 
   this.endModalGroup = this.game.add.group();
   this.endModalGroup.width = 0;
@@ -14,7 +15,7 @@ var EndModal = function(game, parent) {
   this.endModalGroup.y = 0;
   this.endModalGroup.y = (this.game.height-this.endModalGroup.height-200)/2;
 
-  var text = "Congratulation!";
+  var text = "Congratulations!";
   var congratulationTextStyle = { font: "32px Arial", fill: "#ffb62a", align: "center" };
 
   this.congratulationText = this.game.add.text(-120, -130, text, congratulationTextStyle);
