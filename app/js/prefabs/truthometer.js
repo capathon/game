b94c16e68a58218be53dbb02c2eed35b392e18a5
Tariq Ettaji <triqqo@gmail.com>
2015-02-21 01:30:30 +0100

    fixed truthometer bar position

--- app/js/prefabs/truthometer.js
+++ app/js/prefabs/truthometer.js
@@ -17,7 +17,7 @@ Truthometer.prototype.updateHealthbar = function(score) {
     // add first 1px wide unfilled red circle with a radius of 50 at the center (0, 0) of the graphics object
     if (this.health){
         this.health.destroy();
-        this.health = game.add.graphics(29, 118);
+        this.health = game.add.graphics(this.game.width*0.10, this.game.height*0.45);
     }
 
     var pointsToNextLevel = this.game.state.states.Game.levels[this.game.state.states.Game.level + 1].pointsForNextLevel,
